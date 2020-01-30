b756b0669487fb0d6ffbbac05899067e490e24be
dnfvnsxn <dnfvnsxn@naver.com>
2020-01-31 00:25:41 +0900

    setting graphql shema

--- src/server.js
+++ src/server.js
@@ -1,22 +1,14 @@
 require("dotenv").config();
 import { GraphQLServer } from "graphql-yoga";
+import logger from "morgan";
+import schema from "./schema";
 
 const PORT = process.env.PORT || 4000;
 
-const typeDefs = `
-    type Query{
-        hello: String!
-    }
-`;
-
-const resolvers = {
-    Query: {
-        hello: () => "Hi"
-    }
-};
-
 // type 정의와 resolver들이 필요
-const server = new GraphQLServer({ typeDefs, resolvers });
+const server = new GraphQLServer({ schema });
+
+server.express.use(logger("dev"));
 
 server.start({ port: PORT }, () => {
     console.log(`server running on port: http://localhost:${PORT}`);
